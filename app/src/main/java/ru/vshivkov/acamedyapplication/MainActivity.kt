3d91148f8d73f548478729381e54757dccf28d6c
Maksim Vshivkov <maks_w_h@mail.ru>
2020-11-08 12:34:48 +0500

    3 фрагмента, 1 активити С передачей на фрагмент количество переходов между фрагментами (параметр param1) используя arguments

--- app/src/main/java/ru/vshivkov/acamedyapplication/MainActivity.kt
+++ app/src/main/java/ru/vshivkov/acamedyapplication/MainActivity.kt
@@ -1,24 +1,47 @@
 package ru.vshivkov.acamedyapplication
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import ru.vshivkov.acamedyapplication.fragments.FragmentOne
+import ru.vshivkov.acamedyapplication.fragments.FragmentSecond
+import ru.vshivkov.acamedyapplication.fragments.FragmentThird
 
 class MainActivity : AppCompatActivity() {
 
+    private var countChangeFragment: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonForward: Button = findViewById(R.id.buttonForward);
-        buttonForward.setOnClickListener { moveToNextScreen() }
+        val buttonOpenFragmentOne: Button = findViewById(R.id.buttonOpenFragmentOne);
+        buttonOpenFragmentOne.setOnClickListener { changeFragmentContent(FragmentOne()) }
+
+        val buttonOpenFragmentTwo: Button = findViewById(R.id.buttonOpenFragmentTwo);
+        buttonOpenFragmentTwo.setOnClickListener { changeFragmentContent(FragmentSecond()) }
+
+        val buttonOpenFragmentThree: Button = findViewById(R.id.buttonOpenFragmentThree);
+        buttonOpenFragmentThree.setOnClickListener { changeFragmentContent(FragmentThird()) }
     }
 
-    private fun moveToNextScreen() {
-        val intent = Intent(this, SecondActivity::class.java);
-        startActivity(intent);
+    private fun changeFragmentContent(fragment: Fragment) {
+        val bundle = Bundle()
+        bundle.putString("param1", countChangeFragment.toString())
+        bundle.putString("param2", "text param 2")
+        fragment.arguments = bundle
+
+        val transaction = supportFragmentManager.beginTransaction()
+        if (countChangeFragment == 0) {
+            transaction.add(R.id.frame_layout, fragment)
+        } else {
+            transaction.replace(R.id.frame_layout, fragment)
+        }
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+        countChangeFragment++;
     }
 }
